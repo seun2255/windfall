f86813f35700971120a4ce32acb017a7411f6f09
Primal <81525899+Seun2255@users.noreply.github.com>
2023-10-31 01:25:32 +0100

    final corrections

--- app/_assets/icons/icons.js
+++ app/_assets/icons/icons.js
@@ -1,5 +1,5 @@
 import timer from "./timer.svg";
-import canto from "./canto.svg";
+import canto from "./canto.png";
 import metamask from "./metamask.svg";
 import matic from "./matic.svg";
 import ethereum from "./ethereum.svg";
--- app/_utils/formatAmount.js
+++ app/_utils/formatAmount.js
@@ -1,12 +1,12 @@
-//Formats an amount to have 2 d.p for numbers > 1, 3 d.p for numbers < 1 but > 0.001 and returns 0 for amounts
-// less than 0.001
 const formatAmount = (total) => {
   let formattedTotal;
 
-  if (total >= 1) {
-    formattedTotal = total.toFixed(2); // Format to 2 decimal places for values greater than or equal to 1
+  if (total >= 100) {
+    formattedTotal = Math.floor(total); // Remove decimals for values greater than or equal to 100
+  } else if (total >= 1) {
+    formattedTotal = total.toFixed(2); // Format to 2 decimal places for values between 1 and 99.99
   } else if (total >= 0.001) {
-    formattedTotal = total.toFixed(3); // Format to 3 decimal places for values between 0.001 and 1
+    formattedTotal = total.toFixed(3); // Format to 3 decimal places for values between 0.001 and 0.999
   } else {
     formattedTotal = "0"; // Return "0" for values less than 0.001
   }
